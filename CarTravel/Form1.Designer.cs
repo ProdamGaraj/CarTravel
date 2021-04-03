6f765d08859e0e0b131ad28fe67480a7e884204a
prodgaraj@yandex.ru <prodgaraj@yandex.ru>
2021-04-03 21:31:45 +0300

    Добавьте файлы проекта.

---
+++ CarTravel/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace CarTravel
+{
+    partial class CarTravel
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Посчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Год выпуска";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Объём двигателя в cm^3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Стоимость автомобиля в евро";
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Location = new System.Drawing.Point(276, 35);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(17, 13);
+            this.Result.TabIndex = 8;
+            this.Result.Text = "lol";
+            this.Result.Click += new System.EventHandler(this.Result_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(46, 141);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(46, 87);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(276, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "lol";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(276, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "lol";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(276, 187);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "lol";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(245, 171);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "ИТОГО";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(245, 125);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(119, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Таможенная пошлина";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(245, 71);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(121, 13);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Утилизационный сбор";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(245, 19);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(129, 13);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "Стоимость регистрации";
+            // 
+            // CarTravel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 217);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Name = "CarTravel";
+            this.Text = "CarTravel";
+            this.Load += new System.EventHandler(this.CarTravel_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ CarTravel/Form1.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarTravel
+{
+    public partial class CarTravel : Form
+    {
+        public class Car
+        {
+            public int Age { get; set; }
+            public double EngineCapacity { get; set; }
+            public double CarCost { get; set; }
+            public Car(int age, double engineCapacity, double carCost)
+            {
+                Age = age;
+                EngineCapacity = engineCapacity;
+                CarCost = carCost;
+            }
+        }
+        public String CustomsPayment(Car car)
+        {
+            double euro = 90;
+            double dollar = 75;
+            double registrationCost = 0;
+            double recyclingFee = 20000;
+            double customDuties = 0;
+
+            if (car.Age < 3)
+            {
+                recyclingFee *= 0.17;
+                if (car.CarCost > 169000)
+                {
+                    // customDuties calc block
+                    registrationCost = 30000 / euro;
+                    if (car.CarCost * 0.48 < 20 * car.EngineCapacity)
+                    {
+                        customDuties = 20 * car.EngineCapacity;
+                    }
+                    else
+                    {
+                        customDuties = car.CarCost * 0.48;
+                    }
+                }
+                else if (car.CarCost > 84500)
+                {
+                    //registration cost calc block
+                    registrationCost = 23000 / euro;
+                    if (car.CarCost >= 111111)
+                    {
+                        registrationCost = 30000 / euro;//30k rub
+                    }
+                    else if (car.CarCost >= 100000)
+                    {
+                        registrationCost = 27000 / euro;//27k rub
+                    }
+                    else if (car.CarCost >= 88888)
+                    {
+                        registrationCost = 25000 / euro;//25
+                    }
+                    // custom duties calc block
+                    if (car.CarCost * 0.48 < 15 * car.EngineCapacity)
+                    {
+                        customDuties = 15 * car.EngineCapacity;
+                    }
+                    else
+                    {
+                        customDuties = car.CarCost * 0.48;
+                    }
+                }
+                else if (car.CarCost > 42300)
+                {
+                    //registration cost calc block
+                    registrationCost = 12000/euro;
+                    if (car.CarCost >= 77777)
+                    {
+                        registrationCost = 23000 / euro;//23
+                    }
+                    else if (car.CarCost >= 61111)
+                    {
+                        registrationCost = 20000 / euro;//20
+                    }
+                    else if (car.CarCost >= 46666)
+                    {
+                        registrationCost = 15500 / euro;//15500
+                    }
+                    // custom duties calc block
+                    if (car.CarCost * 0.48 < 7.5 * car.EngineCapacity)
+                    {
+                        customDuties = 7.5 * car.EngineCapacity;
+                    }
+                    else
+                    {
+                        customDuties = car.CarCost * 0.48;
+                    }
+                }
+                else if (car.CarCost > 16700)
+                {
+                    //registration cost calc block
+                    registrationCost = 8550 / euro;//8550
+                    if (car.CarCost >= 30000)
+                    {
+                        registrationCost = 12000 / euro;//12
+                    }
+                    //custom duties calc block
+                    if (car.CarCost * 0.48 < 5.5 * car.EngineCapacity)
+                    {
+                        customDuties = 5.5 * car.EngineCapacity;
+                    }
+                    else
+                    {
+                        customDuties = car.CarCost * 0.48;
+                    }
+                }
+                else if (car.CarCost > 8500)
+                {
+                    //registration cost calc block
+                    registrationCost = 3100 / euro;
+                    if (car.CarCost >= 13333)
+                    {
+                        registrationCost = 8550 / euro;
+                    }
+
+                    if (car.CarCost * 0.48 < 3.5 * car.EngineCapacity)
+                    {
+                        customDuties = 3.5 * car.EngineCapacity;
+                    }
+                    else
+                    {
+                        customDuties = car.CarCost * 0.48;
+                    }
+                }
+                else
+                {
+                    //registration cost calc block
+
+                    if (car.CarCost > 5000)
+                    {
+                        registrationCost = 3100 / euro;
+                    }
+                    else if (car.CarCost > 2222)
+                    {
+                        registrationCost = 1550 / euro;
+                    }
+                    else { registrationCost = 775 / euro; }
+                    ////
+                    
+                    if (car.CarCost * 0.54 < 2.5 * car.EngineCapacity)
+                    {
+                        customDuties = 2.5 * car.EngineCapacity;
+                    }
+                    else
+                    {
+                        customDuties = car.CarCost * 0.54;
+                    }
+                }
+            }
+
+            else if (3 <= car.Age && car.Age <= 5)
+            {
+                //registration cost calc block
+                if (car.CarCost >= 111111)
+                {
+                    registrationCost = 30000 / euro;//30k rub
+                }
+                else if (car.CarCost >= 100000)
+                {
+                    registrationCost = 27000 / euro;//27k rub
+                }
+                else if (car.CarCost >= 88888)
+                {
+                    registrationCost = 25000 / euro;//25
+                }
+                else if (car.CarCost >= 77777)
+                {
+                    registrationCost = 23000 / euro;//23
+                }
+                else if (car.CarCost >= 61111)
+                {
+                    registrationCost = 20000 / euro;//20
+                }
+                else if (car.CarCost >= 46666)
+                {
+                    registrationCost = 15500 / euro;//15500
+                }
+                else if (car.CarCost >= 30000)
+                {
+                    registrationCost = 12000 / euro;//12
+                }
+                else if (car.CarCost >= 13333)
+                {
+                    registrationCost = 8550 / euro;//8550
+                }
+                else if (car.CarCost > 5000)
+                {
+                    registrationCost = 3100 / euro;//3100
+                }
+                else if (car.CarCost > 2222)
+                {
+                    registrationCost = 1550 / euro;//1550
+                }
+                else { registrationCost = 775 / euro; }//775                                              
+                                                       ////////
+                recyclingFee *= 0.26;
+                if (car.EngineCapacity > 3001)
+                {
+                    customDuties = car.EngineCapacity * 3.6;
+                }
+                else if (car.EngineCapacity > 2300)
+                {
+                    customDuties = car.EngineCapacity * 3;
+                }
+                else if (car.EngineCapacity > 1800)
+                {
+                    customDuties = car.EngineCapacity * 2.7;
+                }
+                else if (car.EngineCapacity > 1500)
+                {
+                    customDuties = car.EngineCapacity * 2.5;
+                }
+                else if (car.EngineCapacity > 1000)
+                {
+                    customDuties = car.EngineCapacity * 1.7;
+                }
+                else
+                {
+                    customDuties = car.EngineCapacity * 1.5;
+                }
+            }
+
+            else if (car.Age > 5)
+            {
+                //registration cost calc block
+                if (car.CarCost >= 111111)
+                {
+                    registrationCost = 30000 / euro;//30k rub
+                }
+                else if (car.CarCost >= 100000)
+                {
+                    registrationCost = 27000 / euro;//27k rub
+                }
+                else if (car.CarCost >= 88888)
+                {
+                    registrationCost = 25000 / euro;//25
+                }
+                else if (car.CarCost >= 77777)
+                {
+                    registrationCost = 23000 / euro;//23
+                }
+                else if (car.CarCost >= 61111)
+                {
+                    registrationCost = 20000 / euro;//20
+                }
+                else if (car.CarCost >= 46666)
+                {
+                    registrationCost = 15500 / euro;//15500
+                }
+                else if (car.CarCost >= 30000)
+                {
+                    registrationCost = 12000 / euro;//12
+                }
+                if (car.CarCost >= 13333)
+                {
+                    registrationCost = 8550 / euro;//8550
+                }
+                if (car.CarCost > 5000)
+                {
+                    registrationCost = 3100 / euro;
+                }
+                else if (car.CarCost > 2222)
+                {
+                    registrationCost = 1550 / euro;
+                }
+                else { registrationCost = 775 / euro; }
+                                                    /////////
+                if (car.EngineCapacity > 3001)
+                {
+                    customDuties = car.EngineCapacity * 5.7;
+                }
+                else if (car.EngineCapacity > 2300)
+                {
+                    customDuties = car.EngineCapacity * 5;
+                }
+                else if (car.EngineCapacity > 1800)
+                {
+                    customDuties = car.EngineCapacity * 4.8;
+                }
+                else if (car.EngineCapacity > 1500)
+                {
+                    customDuties = car.EngineCapacity * 3.5;
+                }
+                else if (car.EngineCapacity > 1000)
+                {
+                    customDuties = car.EngineCapacity * 3.2;
+                }
+                else
+                {
+                    customDuties = car.EngineCapacity * 3;
+                }
+            }
+
+            customDuties *= euro;
+            return registrationCost.ToString()+ "|" + recyclingFee.ToString() + "`" + customDuties.ToString() + "!" + (registrationCost + recyclingFee + customDuties).ToString();
+        }
+
+
+        public CarTravel()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double releaseYear = Convert.ToDouble(textBox1.Text);
+            double engineCapacity = Convert.ToDouble(textBox2.Text);
+            double carCost = Convert.ToDouble(textBox3.Text) ;
+            int age = Convert.ToInt32(DateTime.Now.Year-releaseYear);
+            Car car = new Car(age, carCost, engineCapacity);
+            string output = CustomsPayment(car);
+            Result.Text = output.Substring(0,output.IndexOf("|"));
+            output=output.Remove(0, output.IndexOf("|")+1);
+            label5.Text = output.Substring(0,output.IndexOf("`"));
+            output=output.Remove(0, output.IndexOf("`")+1);
+            label6.Text = output.Substring(0,output.IndexOf("!"));
+            output=output.Remove(0, output.IndexOf("!")+1);
+            label7.Text = output.Substring(0,output.Length);
+
+        }
+        private void CarTravel_Load(object sender, EventArgs e) { }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Result_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
